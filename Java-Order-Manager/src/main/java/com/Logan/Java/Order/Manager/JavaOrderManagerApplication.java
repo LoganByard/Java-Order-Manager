1d5af9409fe4d8fae325bbb1db6c00b97a04dbcc
LoganByard <lbyard@wisc.edu>
2025-05-19 07:28:53 -0500

    initial commit

---
+++ Java-Order-Manager/src/main/java/com/Logan/Java/Order/Manager/JavaOrderManagerApplication.java
@@ -0,0 +1,13 @@
+package com.Logan.Java.Order.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOrderManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOrderManagerApplication.class, args);
+	}
+
+}
---
+++ Java-Order-Manager/src/test/java/com/Logan/Java/Order/Manager/JavaOrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Logan.Java.Order.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
